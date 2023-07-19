59b5bd366da11b705b83b71d21a8eb2ba2540cc8
tatermysalad <rosette.rural0c@icloud.com>
2023-07-19 20:33:41 +1000

    add update and delete methods to category endpoint

--- src/controllers/category_controller.py
+++ src/controllers/category_controller.py
@@ -1,3 +1,4 @@
+from datetime import date
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 import functools
@@ -31,3 +32,43 @@ def get_categories():
 def get_category(id):
     category = db.session.scalar(db.select(Category).filter_by(category_id=id))
     return category_schema.dump(category)
+
+@category_bp.route('/', methods=["POST"])
+@jwt_required()
+@authorise_as_admin
+def create_category():
+    body_data = request.get_json()
+    category = Category(
+        name = body_data.get("name"),
+        description = body_data.get("description"),
+        date = date.today(),
+    )
+    db.session.add(category)
+    db.session.commit()
+    return category_schema.dump(category), 201
+
+@category_bp.route('/<int:id>', methods=["PUT", "PATCH"])
+@jwt_required()
+@authorise_as_admin
+def update_category(id):
+    body_data = request.get_json()
+    category = db.session.scalar(db.select(Category).filter_by(category_id=id))
+    if category:
+        category.name = body_data.get("name") or category.name,
+        category.description = body_data.get("description") or category.description
+        db.session.commit()
+        return category_schema.dump(category), 201
+    else:
+        return jsonify(message="category not found"), 404
+    
+@category_bp.route('/<int:id>', methods=["DELETE"])
+@jwt_required()
+@authorise_as_admin
+def delete_category(id):
+    category = db.session.scalar(db.select(Category).filter_by(category_id=id))
+    if category:
+        db.session.delete(category)
+        db.session.commit()
+        return jsonify(message=f"Category with id='{id}' deleted"), 200
+    else:
+        return jsonify(message=f"Category not found with id='{id}'"), 404
